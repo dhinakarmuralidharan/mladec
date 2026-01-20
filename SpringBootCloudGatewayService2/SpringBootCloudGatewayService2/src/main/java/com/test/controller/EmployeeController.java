6dc09d2d57faf4e72384e03309d154d7dad8fa54
dhinakarmuralidharan@gmail.com <dhinakarmuralidharan@gmail.com>
2026-01-20 10:06:38 +0530

    SpringBoot Cloud Gateway

---
+++ SpringBootCloudGatewayService2/SpringBootCloudGatewayService2/src/main/java/com/test/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController 
+{
+	@GetMapping("/test")
+	public String employeeAPI()
+	{
+		return "this is simple cloud employee service";
+	}
+}
